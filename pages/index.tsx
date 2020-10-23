64545240403ef11a17c8a9341a3d68c6187dff74
maratib <maratib@maratib.local>
2020-10-23 21:49:41 +0500

    intial

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example" meta="Home Page">
-    <h1>Hello Next.js 1234567 👋</h1>
+    <h1>Hello Next.js 12345678 👋</h1>
     <p>
       <Link href="/about">
         <a>About</a>
